fb2948555b532dcda27d35b996034049d936e200
Othavio-web <66527646+Othavio-web@users.noreply.github.com>
2024-04-13 22:20:47 -0300

    Add files via upload

--- cursoSpringRestApi/src/main/java/curso/api/rest/controle/IndexControler.java
+++ cursoSpringRestApi/src/main/java/curso/api/rest/controle/IndexControler.java
@@ -1,5 +1,11 @@
 package curso.api.rest.controle;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +26,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
 
 import curso.api.rest.model.Usuario;
 import curso.api.rest.repository.UsuarioRepository;
@@ -55,10 +63,28 @@ public class IndexControler {
 		return "ok";
 	}
 	@PutMapping(value ="/", produces ="aplication/json")
-	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) throws Exception{
 		for(int pos = 0;pos<usuario.getTelefones().size();pos++) {
 			usuario.getTelefones().get(pos).setUsuario(usuario);
 		}
+		URL url =new  URL("https://viacep.com.br/ws/"+usuario.getCep()+"/json/");
+		URLConnection connection = url.openConnection();
+		InputStream is = connection.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+		
+		String cep="";
+		StringBuilder jsonCep = new StringBuilder();
+		
+		while((cep=br.readLine())!=null) {
+			jsonCep.append(cep);
+		}
+		Usuario userAux = new Gson().fromJson(jsonCep.toString(), Usuario.class);
+		usuario.setSenha(userAux.getSenha());
+		usuario.setLogradouro(userAux.getLogradouro());
+		usuario.setComplemento(userAux.getComplemento());
+		usuario.setBairro(userAux.getBairro());
+		usuario.setLocalidade(userAux.getLocalidade());
+		usuario.setUf(userAux.getUf());
 		Usuario userTempo = usuarioRepository.findUserByLogin(usuario.getLogin());
 		if(!userTempo.getSenha().equals(usuario.getSenha())) {
 			String senhaCriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
